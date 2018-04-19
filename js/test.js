d2eb42a233f21ad6944928cb226db9b79cf85c58
administrator2 <administrator2@NF1657>
2018-04-19 17:17:00 +0900

    change

--- js/test.js
+++ js/test.js
@@ -1,4 +1,5 @@
 $(function() {
+
 	$('.tab li').click(function() {
 		var index = $('.tab li').index(this);
 		$('.content li').css('display','none');
@@ -6,4 +7,12 @@ $(function() {
 		$('.tab li').removeClass('select');
 		$(this).addClass('select')
 	});
+
+	$(".exit input").change(function() {
+		$('.related input').prop('disabled', !$(".exit input").prop('checked'));
+		$('.related').css({ color: $(".exit input").prop('checked') ? 'black' : 'silver' });
+		$('.content span').next('p')[$(".exit input").prop('checked') ? 'show' : 'hide']();
+	});
+	$(".exit input").trigger('change');
+
 });
