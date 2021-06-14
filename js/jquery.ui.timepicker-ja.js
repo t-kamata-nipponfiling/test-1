02d93dd316c50e6884f252f6e0f1d2bc605f31d2
t.kamata <t.kamata@NF1657.TOKYO.nipponfiling.co.jp>
2021-06-14 11:11:08 +0900

    test

--- js/jquery.ui.timepicker-ja.js
+++ js/jquery.ui.timepicker-ja.js
@@ -1,16 +1,18 @@
 /* Japanese initialisation for the jQuery time picker plugin. */
 /* Written by Bernd Plagge (bplagge@choicenet.ne.jp). */
+
 jQuery(function($){
 	$.timepicker.regional['ja'] = {
-		// Options
+		// options
 		timeSeparator: ':',				// The character to use to separate hours and minutes (default: ':')
+		timeWithoutSeparator: true,     // Accept time input without seperator (default: true).
+		periodSeparator: ' ',			// The character to use to separate the time from the time period
+		showPeriod: false,				// Define whether or not to show AM/PM with selected time (default: false)
+		showPeriodLabels: true,			// Define if the AM/PM labels on the left are displayed (default: true)
 		showLeadingZero: true,			// Define whether or not to show a leading zero for hours < 10 (default: true)
 		showMinutesLeadingZero: true,	// Define whether or not to show a leading zero for minutes < 10 (default: true)
-		showPeriod: true,				// Define whether or not to show AM/PM with selected time (default: false)
-		showPeriodLabels: true,			// Define if the AM/PM labels on the left are displayed (default: true)
-		periodSeparator: ' ',			// The character to use to separate the time from the time period
 		altField: '',					// Define an alternate input to parse selected time to
-		defaultTime: '',				// Used as default time when input field is empty or for inline timePicker
+		defaultTime: 'now',				// Used as default time when input field is empty or for inline timePicker
 										// (set to 'now' for the current time, '' for no highlighted time, default value: now)
 		
 		// trigger options
@@ -18,22 +20,26 @@ jQuery(function($){
 										// 'focus': when the input gets focus, 'button' when the button trigger element is clicked,
 										// 'both': when the input gets focus and when the button is clicked
 		button: null,					// jQuery selector that acts as button trigger (ex: '#trigger_button')
+		showAnim: 'slideIn',			// Name of jQuery animation for popup
+		duration: 400,
+		showOptions: {},				// Options for enhanced animations
+		appendText: '',					// Display text following the input box, e.g. showing the format
 		
-		// Localization
-		hourText: '時',					// Define the locale text for "Hours"
+		// localization
+		hourText: '時',					// Define the locale text for "Hour"
 		minuteText: '分',				// Define the locale text for "Minute"
-		amPmText: ['AM', 'PM'],			// Define the locale text for periods
+		amPmText: ['AM', 'PM'],			// Define the locale text for "AM / PM"
 		
-		// Position
+		// position
 		myPosition: 'left top',			// Corner of the dialog to position, used with the jQuery UI Position utility if present
 		atPosition: 'left bottom',		// Corner of the input to position
 		
-		// Events
-		beforeShow: '',					// Callback function executed before the timepicker is rendered and displayed
-		onSelect: '',					// Define a callback function when an hour / minutes is selected
-		onClose: '',					// Define a callback function when the timepicker is closed
-		onHourShow: '',					// Define a callback to enable / disable certain hours (ex: function onHourShow(hour))
-		onMinuteShow: '',				// Define a callback to enable / disable certain minutes (ex: function onMinuteShow(hour, minute))
+		// events
+		beforeShow: null,				// Callback function executed before the timepicker is rendered and displayed
+		onSelect: null,					// Define a callback function when an hour / minutes is selected
+		onClose: null,					// Define a callback function when the timepicker is closed
+		onHourShow: null,				// Define a callback to enable / disable certain hours (ex: function onHourShow(hour))
+		onMinuteShow: null,				// Define a callback to enable / disable certain minutes (ex: function onMinuteShow(hour, minute))
 		
 		// custom hours and minutes
 		hours: {
@@ -49,8 +55,9 @@ jQuery(function($){
 		rows: 4,						// Number of rows for the input tables, minimum 2, makes more sense if you use multiple of 2
 		showHours: true,				// Define if the hours section is displayed or not (Set to false to get a minute only dialog)
 		showMinutes: true,				// Define if the minutes section is displayed or not (Set to false to get an hour only dialog)
+		optionalMinutes: false,         // optionally parse inputs of whole hours with minutes omitted
 		
-		// Min and Max time
+		// min and max time
 		minTime: {						// Set the minimum time selectable by the user, disable hours and minutes
 			hour: 0,					// previous to min time
 			minute: 0
@@ -62,11 +69,11 @@ jQuery(function($){
 		
 		// buttons
 		showCloseButton: false,			// shows an OK button to confirm the edit
-		closeButtonText: '閉じる',		// Text for the confirmation button (ok button)
+		closeButtonText: 'Done',		// Text for the confirmation button (ok button)
 		showNowButton: false,			// Shows the 'now' button
-		nowButtonText: '現在時刻',		// Text for the now button
+		nowButtonText: 'Now',			// Text for the now button
 		showDeselectButton: false,		// Shows the deselect time button
-		deselectButtonText: '選択解除'	// Text for the deselect button
+		deselectButtonText: 'Deselect'	// Text for the deselect button
 	}
 	$.timepicker.setDefaults($.timepicker.regional['ja']);
 });
